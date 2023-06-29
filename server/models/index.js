4cddf5407042d74ef8f6babbafdd9b854e847daf
PhilRug <ruggiero.pj@gmail.com>
2023-06-29 19:17:32 -0400

    index model add/server dependancies

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Artwork = require('./Artwork');
+const Category = require('./Category');
+const Order = require('./Order');
+
+module.exports = { User, Artwork, Category, Order };
