41fd1d539a3749e98a5fa1a26afd93db271c7307
Jason Lewis <pensfan5050@gmail.com>
2023-07-03 15:52:13 -0400

    added code to tests

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -14,3 +14,23 @@ beforeAll(() => {
 afterAll(() => {
   // Global teardown logic after running tests
 });
+
+/* // Perform any necessary test setup and configuration here
+
+// Example: Configuring an Enzyme adapter for React component testing
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+// Example: Configuring Jest's global setup and teardown functions
+beforeAll(() => {
+  // Global setup logic before running tests
+  console.log("Running global setup");
+});
+
+afterAll(() => {
+  // Global teardown logic after running tests
+  console.log("Running global teardown");
+});
+ */
