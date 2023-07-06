36724de512d5b96251d6966e2739b3c9eb7daac3
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-07-06 19:11:07 -0400

    added component folder for deep ai

---
+++ client/src/components/ImageGenerator/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+
+//need to call back end route
+
+// need to get user text
+
+//need to display generated image
+
+// example >
+
+return (
+    <div>
+     
+    
+      {imageUrl && <img src={imageUrl} alt="Generated Image" />}
+    </div>
+  );
+
+export default DeepAIComponent;
