b58554f4756b7c8eaa6d21fcbae9d5eefcdb9478
CJB14 <119978168+CJB14@users.noreply.github.com>
2023-07-13 13:55:22 -0400

    .env file and connection update to fix depo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017'
-);
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-module.exports = mongoose.connection;
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB database.');
+});
+
+module.exports = db;
